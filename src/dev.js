f89107cafe0ff7088ff4717f0018a24003b32aa1
Albert Kawmi <albert.kawmi@gmail.com>
2018-05-07 01:29:17 +0100

    fix race conditions in dev.js

--- src/dev.js
+++ src/dev.js
@@ -3,30 +3,52 @@ const build = require('./build')
 const livereload = require('livereload');
 const serve = require('serve')
 
-module.exports = ({ env }) => {
+module.exports = async ({ env }) => {
   const devBuild = () => build({ env })
-  devBuild()
+  await devBuild()
 
   const cwd = process.cwd()
+  const dist = `${cwd}/dist`
 
-  const lrServer = livereload.createServer();
-  lrServer.watch(`${cwd}/dist`);
+  await initLivereload(dist)
 
-  const devServer = serve(`${cwd}/dist`, { open: true, clipless: true }) // eslint-disable-line
+  await initFileWatching(cwd, devBuild)
 
-  const watcher = chokidar.watch(cwd, {
-    ignoreInitial: true,
-    ignored: [
-      '/dist',
-      '/node_modules',
-      '/.git'
-    ].map(path => cwd.concat(path))
+  const devServer = serve(dist, { open: true, clipless: true }) // eslint-disable-line
+}
+
+function initLivereload(dist) {
+  return new Promise((resolve) => {
+    const lrServer = livereload.createServer(
+      { delay: 500 },
+      () => {
+        lrServer.watch(dist)
+        resolve()
+      }
+    )
   })
+}
 
-  watcher
-    .on('ready', () => console.log('Watching for changes in', cwd))
-    .on('change', devBuild)
-    .on('add', devBuild)
-    .on('unlink', devBuild)
-    .on('unlinkDir', devBuild)
+function initFileWatching(cwd, devBuild) {
+  return new Promise((resolve) => {
+    const watcher = chokidar.watch(cwd, {
+      ignoreInitial: true,
+      ignored: [
+        '/dist',
+        '/node_modules',
+        '/.git'
+      ].map(path => cwd.concat(path))
+    })
+
+    watcher
+      .on('ready', () => {
+        // eslint-disable-next-line
+        console.log('Watching for changes in', cwd)
+        resolve()
+      })
+      .on('change', devBuild)
+      .on('add', devBuild)
+      .on('unlink', devBuild)
+      .on('unlinkDir', devBuild)
+  })
 }
